e443322ed39483991a71ebf5355862a5ebde0186
Anvar <anvar102b@gmail.com>
2021-04-07 11:40:21 +0500

    Initial Commit

---
+++ CompositionalLayoutTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutTest
+//
+//  Created by Anvar Basharov on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
